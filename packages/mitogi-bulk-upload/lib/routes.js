25f74229ab580374ca2f5c85b15676655b17be08
mxchelle <michelle@Michelles-MacBook-Pro.local>
2016-08-30 02:07:42 -0400

    picker

--- packages/mitogi-bulk-upload/lib/routes.js
+++ packages/mitogi-bulk-upload/lib/routes.js
@@ -4,20 +4,3 @@ FlowRouter.route('/users/bulk', {
     BlazeLayout.render("layout", {main: "bulkUsers"});
   }
 });
-
-
-
-
-// Account approved email
-Picker.route('/email/bulk-add/?', function(params, req, res, next) {
-  var user = typeof params.id === "undefined" ? Meteor.users.findOne() : Meteor.users.findOne(params.id);
-  var emailProperties = {
-    changePwdUrl: FlowRouter.path("changePwd"),
-    username: Users.getUserName(user),
-    password: Random.id(5),
-    siteTitle: Settings.get('title'),
-    siteUrl: Telescope.utils.getSiteUrl()
-  };
-  var html = Telescope.email.getTemplate('emailAdd')(emailProperties);
-  res.end(Telescope.email.buildTemplate(html));
-});
