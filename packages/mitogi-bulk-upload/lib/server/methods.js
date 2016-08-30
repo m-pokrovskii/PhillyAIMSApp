ad858f88efda592e9972770aafc18be75611b186
mxchelle <michelle@Michelles-MacBook-Pro.local>
2016-08-30 12:27:26 -0400

    deleted try

--- packages/mitogi-bulk-upload/lib/server/methods.js
+++ packages/mitogi-bulk-upload/lib/server/methods.js
@@ -7,29 +7,27 @@
 
     for ( let i = 0; i < data.length; i++ ) {
       let item   = data[ i ];
+      console.log(item);
       try{
         var password = Random.id(5),
         username = item["First Name"]+item["Last Name"],
         email = item["Email"];
         Accounts.createUser({username: username, password : password, email: email });
-        
-        try{
-          var communityName = Settings.get('title','Philly AIMS'),
-              emailSubject = 'Access The Philly AIMS Portal',
-              emailProperties = {
-                username : username,
-                password : password,
-                //email : email,
-                changePwdUrl : FlowRouter.path("changePwd"),
-                siteUrl: Settings.get("siteUrl"),
-              };
+      
+        var communityName = Settings.get('title','Philly AIMS'),
+            emailSubject = 'Access The Philly AIMS Portal',
+            emailProperties = {
+              username : username,
+              password : password,
+              //email : email,
+              changePwdUrl : FlowRouter.path("changePwd"),
+              siteUrl: Settings.get("siteUrl"),
+            };
 
-          Meteor.setTimeout(function () {
-            Telescope.email.buildAndSend(email, emailSubject, 'emailAdd', emailProperties);
-          }, 1);
-        }
-        catch (exception){
-        }
+        Meteor.setTimeout(function () {
+          Telescope.email.buildAndSend(email, emailSubject, 'emailAdd', emailProperties);
+        }, 1);
+        
         success.push("Made account for "+ email);
       }
       catch (exception){
