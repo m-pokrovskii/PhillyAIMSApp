b8514e22b96867df710b0a9eec54a73b8fea0830
mxchelle <michelle@Michelles-MacBook-Pro.local>
2016-08-31 15:43:25 -0400

    works now

--- packages/mitogi-bulk-upload/lib/server/methods.js
+++ packages/mitogi-bulk-upload/lib/server/methods.js
@@ -5,34 +5,48 @@
     var error = new Array();
     var success = new Array();
 
-    for ( let i = 0; i < data.length; i++ ) {
-      let item   = data[ i ];
-      console.log(item);
-      try{
-        var password = Random.id(5),
-        username = item["First Name"]+item["Last Name"],
-        email = item["Email"];
-        Accounts.createUser({username: username, password : password, email: email });
+    for ( var i = 0; i < data.length; i++ ) {
       
-        var communityName = Settings.get('title','Philly AIMS'),
-            emailSubject = 'Access The Philly AIMS Portal',
-            emailProperties = {
-              username : username,
-              password : password,
-              //email : email,
-              changePwdUrl : FlowRouter.path("changePwd"),
-              siteUrl: Settings.get("siteUrl"),
-            };
-
-        Meteor.setTimeout(function () {
-          Telescope.email.buildAndSend(email, emailSubject, 'emailAdd', emailProperties);
-        }, 1);
-        
-        success.push("Made account for "+ email);
-      }
-      catch (exception){
-        error.push("Could not make account for "+email+". "+exception);
-      }
+          //Meteor.setTimeout(function () {
+            try{
+              var item   = data[ i ];
+              console.log("item");
+               console.log(item);
+      
+   
+              var password = Random.id(5),
+              username = item["First Name"]+item["Last Name"],
+              email = item["Email"]+'';
+              var result = Accounts.createUser({username: username, password : password, email: email });
+              console.log("result: "+ result);
+             if(Meteor.users.find({_id:result})){
+                var myUsername = username+'';
+                var myPassword= password+'';
+                var myEmail= email+'';
+                var communityName = Settings.get('title','Philly AIMS'),
+                emailSubject = 'Access The Philly AIMS Portal';
+                var emailProperties = {
+                  username : myUsername,
+                  password : myPassword,
+                  //email : email,
+                  changePwdUrl : FlowRouter.path("changePwd"),
+                  siteUrl: Settings.get("siteUrl"),
+                };
+                console.log(emailProperties);
+                Telescope.email.buildAndSend(myEmail, emailSubject, 'emailAdd', emailProperties);
+                console.log(myUsername+" "+myPassword);
+                success.push("Made account for "+ myEmail);
+              }//end if
+              else{
+                error.push("Could not make account for "+email+". "+exception);
+              }//end else
+            }//end try
+            catch (exception){
+              error.push("Could not make account for "+email+". "+exception);
+            }
+        //}, 1);
+          
+          
 
       
     }//end for
