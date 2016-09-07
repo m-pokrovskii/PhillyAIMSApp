93d66b5927eeaa8772428811584f8f09706420b0
mxchelle <michelle@Michelles-MacBook-Pro.local>
2016-09-07 15:09:44 -0400

    delete thumbnails

--- packages/mitogi-upload/lib/both/methods/files.js
+++ packages/mitogi-upload/lib/both/methods/files.js
@@ -181,7 +181,7 @@
               array.push({Key: newKey + ".webm",
                //ThumbnailPattern: "thumbs-" + newKey,
                PresetId: '1351620000001-100240', //Webm 720p
-               ThumbnailPattern: 'thumbnails/'+newKey+'-{count}', 
+               //ThumbnailPattern: 'thumbnails/'+newKey+'-{count}', 
                Rotate: 'auto'
               });
              }
